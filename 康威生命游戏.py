15d1bde667ef4f8b7b99418703384613a77f11ed
XiongWenhao404 <132863615+XiongWenhao404@users.noreply.github.com>
2023-07-18 17:25:44 +0800

    Update 康威生命游戏.py

--- 康威生命游戏.py
+++ 康威生命游戏.py
@@ -3,23 +3,22 @@
 import tkinter as tk
 import random
 
-from SPECIAL_LIST import SPECIAL_LIST  # �������������
+from SPECIAL_LIST import SPECIAL_LIST  # 导入特殊参数表
 
-#  �������ӣ�Ϊ0ʱ���������������Ϊ1ʱʹ���ض�������������������
+#  设置种子，为0时将创建随机生命，为1时使用特定参数表创建特殊生命
 SEED = 0
 
-# �������ӣ��޸ĵ�ͼ���Ա�
+# 依据种子，修改地图属性表
 PROPERTY_LIST = [20, 400, 400, 500]
 if SEED == 1:
     PROPERTY_LIST = SPECIAL_LIST[SEED - 1][0]
 
-# ���ݵ�ͼ���Ա���������ԣ�����Ϊ�������С�����ڿ������ڸߣ�ˢ��ʱ��
-SIZE, WINDOW_WIDTH, WINDOW_HEIGHT, DELAY = PROPERTY_LIST
-WIDTH_LEN = WINDOW_WIDTH // SIZE
-HEIGHT_LEN = WINDOW_HEIGHT // SIZE
+SIZE, WINDOW_WIDTH, WINDOW_HEIGHT, DELAY = PROPERTY_LIST  # 依据地图属性表导入各属性，依次为：方块大小，窗口宽，窗口高，刷新时长
+WIDTH_LEN = WINDOW_WIDTH // SIZE  # 换算出横向方块数量
+HEIGHT_LEN = WINDOW_HEIGHT // SIZE  # 换算出纵向方块数量
 
 
-# ��ʼ�������������ڻ��������
+# 初始化康威生命并在画布上描绘
 class LiVes:
     def __init__(self, seed):
         self.cells = [[random.choice([0, 1]) for _ in range(WIDTH_LEN)] for _ in
@@ -60,7 +59,7 @@ class LiVes:
                                         fill=self.colorSwitch[self.cells[i][j]])
 
 
-# ִ�������ݽ�����ֹͣ�ݽ������¿�ʼ�µ�һ������
+# 执行生命演进并在停止演进后重新开始新的一轮生命
 class Conway:
     def __init__(self, master):
         self.master = master
@@ -85,29 +84,29 @@ class Conway:
             self.restart()
         self.master.after(DELAY, self.update)
 
-    # ֹͣ�ݽ������¿�ʼ�µ�һ������
+    # 停止演进后重新开始新的一轮生命
     def restart(self):
         self.master.destroy()
         newRoot = tk.Tk()
         newGame = Conway(newRoot)
-        print(newGame.round)  # ��������䣬�����������档����Ϊδʹ�þֲ�����'newGame'��ֵ����newGame��������ģ����ʹ�ô�����������档
+        # print(newGame.round)  # 无意义语句，用来消除警告。警告为未使用局部变量'newGame'的值，但newGame是有意义的，因此使用此语句消除警告。
         root.mainloop()
 
 
-# ������ʼ��������Ļ����
+# 将窗口始终置于屏幕中央
 def centerWindow(window, window_width=WINDOW_WIDTH, window_height=WINDOW_HEIGHT):
-    # ��ȡ��Ļ�Ŀ��Ⱥ͸߶�
+    # 获取屏幕的宽度和高度
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
-    # ���㴰�ڵĿ��Ⱥ͸߶�
-    # ���㴰�����Ͻǵ�����ʹ�������Ļ�м�
+    # 计算窗口的宽度和高度
+    # 计算窗口左上角的坐标使其居于屏幕中间
     x = (screen_width - window_width) // 2
     y = (screen_height - window_height) // 2
-    # ���ô��ڵĴ�С��λ��
+    # 设置窗口的大小和位置
     window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 
-# ����
+# 运行
 if __name__ == "__main__":
     root = tk.Tk()
     game = Conway(root)
